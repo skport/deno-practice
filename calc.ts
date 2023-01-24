46790b58d6d37e7414b2f99b07b3b1bae317bb41
skport <45557654+skport@users.noreply.github.com>
2023-01-25 00:08:00 +0900

    Add operators to calc

--- calc.ts
+++ calc.ts
@@ -3,10 +3,26 @@ import { parse } from "https://deno.land/std@0.173.0/flags/mod.ts";
 const parsedArgs = parse(Deno.args);
 
 let a = 0
+let operator = ''
 for (const i of parsedArgs._) {
-  if (isNaN(+i)) {
-    throw new Error("Non-numeric characters found in parameters.");
-  }
-  a += +i
+  if (i == '+' || i == '-' || i == 'x' || i == '/') {
+    operator = i
+  } else {
+    if (isNaN(+i)) {
+      throw new Error('Non-numeric characters found in parameters.');
+    }
+    if (operator == '+') {
+      a += +i
+    } else if (operator == '-') {
+      a -= +i
+    } else if (operator == 'x') {
+      a *= +i
+    } else if (operator == '/') {
+      a /= +i
+    } else {
+      a += +i
+    }
+    operator = ''
+  }  
 }
 console.log(a)
--- readme.md
+++ readme.md
@@ -3,6 +3,14 @@
 ### scripts
 
 #### calc
-input: ```deno run calc.ts 1 2 3 4 5```
+input: ```deno run calc.ts 1 + 2 + 3 + 4 + 5```
 
 output: ```15```
+
+input: ```deno run calc.ts 1 + 2 - 3```
+
+output: ```0```
+
+input: ```deno run calc.ts 1 x 2 x 3 / 4```
+
+output: ```1.5```
