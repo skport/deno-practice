0b4421aa60e9e3ac85088c00add2749391515203
skport <45557654+skport@users.noreply.github.com>
2023-02-24 19:57:09 +0900

    Add official example for bench

---
+++ bench.ts
@@ -0,0 +1,31 @@
+Deno.bench("URL parsing", () => {
+  new URL("https://deno.land");
+});
+
+Deno.bench("Async method", async () => {
+  await crypto.subtle.digest("SHA-256", new Uint8Array([1, 2, 3]));
+});
+
+Deno.bench({
+  name: "Long form",
+  fn: () => {
+    new URL("https://deno.land");
+  },
+});
+
+Deno.bench({
+  name: "Date.now()",
+  group: "timing",
+  baseline: true,
+  fn: () => {
+    Date.now();
+  },
+});
+
+Deno.bench({
+  name: "performance.now()",
+  group: "timing",
+  fn: () => {
+    performance.now();
+  },
+});
